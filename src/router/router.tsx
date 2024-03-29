93842e356a11d48760df1aa102c4cf2ab6e52f67
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-03-29 09:25:34 +0100

    up

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,25 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import PlanetsListPage from "../pages/PlanetsListPage";
 import ExemplePage1 from "../pages/ExemplePage1";
 import ExemplePage2 from "../pages/ExemplePage2";
-import AppLayout from "../layouts/AppLayout";
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <PlanetsListPage />,
-      },
-      {
-        path: "/example_1",
-        element: <ExemplePage1 />,
-      },
-      {
-        path: "/example_2",
-        element: <ExemplePage2 />,
-      },
-    ],
+    path: "/",
+    element: <PlanetsListPage />,
+  },
+  {
+    path: "/example_1",
+    element: <ExemplePage1 />,
+  },
+  {
+    path: "/example_2",
+    element: <ExemplePage2 />,
   },
 ]);
 
