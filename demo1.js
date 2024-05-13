ee684a4fc664da38f1a92d05e91ae339a0848b58
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-05-13 11:35:34 +0200

    filter

--- demo1.js
+++ demo1.js
@@ -1,2 +1,15 @@
-var bonjour = "au revoir";
-console.log(bonjour);
+const demo = [
+  {
+    title: "toto",
+  },
+  {
+    title: "tata",
+  },
+  {
+    title: "bruno",
+  },
+];
+
+const filteredTab = demo.filter((item) => item.title[0] === "t");
+
+console.log(filteredTab);
